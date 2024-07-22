e02c7e1145f300ef0f92538078b08f50983fb49e
nmvbxcz <455453015@qq.com>
2024-07-22 16:13:32 +0800

    Update lgcatsw.m

--- lgcatsw.m
+++ lgcatsw.m
@@ -1,6 +1,6 @@
-function [data] = lgcatsw(img,radius,iter)
+function [data] = lgcatsw(img, radius, pc)
     [h,w,c] = size(img);
-    local_feature = ls_box(img,radius,iter);
-    global_feature = gs_box(img, radius, iter); 
+    local_feature = ls_box(img,radius);
+    global_feature = gs_box(img, radius, pc); 
     data = cat(3,local_feature,global_feature);
 end
